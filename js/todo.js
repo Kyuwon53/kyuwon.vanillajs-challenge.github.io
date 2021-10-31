524ba0c83a89f75432013753d8fdeaa9f1ce0f32
kyuwon53 <lkw2v@naver.com>
2021-11-01 01:42:05 +0900

    [Fix] todo list 날짜추가

--- js/todo.js
+++ js/todo.js
@@ -3,6 +3,7 @@ const todoList = document.querySelector("#todo-list");
 const todoInput = todoForm.querySelector("input");
 
 const TODOS_KEY = "todos";
+const TEXT_LINE = "text-line";
 
 let toDos = [];
 
@@ -13,42 +14,60 @@ function saveToDos(){
 function deleteToDo (event) {
   const li = event.target.parentElement;
   li.remove();
-  toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
+  toDos = toDos.filter(toDo => toDo.liId !== parseInt(li.liId));
   saveToDos();
 }
 
 function checkToDo (event) {
   const li = event.target.parentElement;
-  li.style.textDecoration = 'line-through';
+  const className = li.getAttribute("class");
+  console.log(className);
+  if (className === TEXT_LINE) {
+    li.classList.remove(TEXT_LINE);
+  } else {
+    li.classList.add(TEXT_LINE);
+  }
 }
 
 function paintToDo(newTodo) {
+  const ul = document.createElement("ul");
   const li = document.createElement("li");
-  li.id = newTodo.id;
+  const toDoDate = document.createElement("span");
+
+  toDoDate.innerText = newTodo.ulId;
+
+  ul.appendChild(li);
+  li.liId = newTodo.liId;
+
   const span = document.createElement("span");
   span.innerText = newTodo.text;
 
   const button = document.createElement("button");
   button.innerText = "X";
   button.classList.add("button");
+  button.classList.add("delete");
   button.addEventListener("click", deleteToDo);
+
+
+  li.addEventListener("click", checkToDo);
   li.appendChild(span);
+  li.appendChild(toDoDate);
   li.appendChild(button);
-  todoList.appendChild(li);
+  todoList.appendChild(ul);
 }
 
 function handleToDoSubmit(event) {
-  //const date = new Date();
-  //const year = date.getFullYear();
-  //const month = date.getMonth();
-  //const day = date.getDate();
-  //ulId: `${year}${month}${day}`,
   event.preventDefault();
   const newTodo = todoInput.value;
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
   todoInput.value = "";
   const newTodoObj = {
     text: newTodo,
-    id: Date.now(),
+    ulId: `${year}${month}${day}`,
+    liId: Date.now(),
   };
   toDos.push(newTodoObj);
   paintToDo(newTodoObj);
